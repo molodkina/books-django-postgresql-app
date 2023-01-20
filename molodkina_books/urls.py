4ff06ac1cad2436ce2b94650e516e6982f5217e7
molodkina <xenia.molodkina@gmail.com>
2023-01-20 19:37:20 +0000

    Call filter_queryset manually in book view Get controller.

--- molodkina_books/urls.py
+++ molodkina_books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 from molodkina_books import views
-from molodkina_books.views import  BookViewSet, CountryViewSet, BookFilterViewSet, AuthorViewSet
+from molodkina_books.views import BookViewSet, CountryViewSet, BookFilterViewSet, AuthorViewSet
 
 default_router = routers.DefaultRouter()
 default_router.register(r'authors', AuthorViewSet, basename='authors')
-default_router.register(r'books', BookFilterViewSet, basename='books')
+# default_router.register(r'books', BookFilterViewSet, basename='books')
+# default_router.register(r'books', BookFilterViewSetMVS, basename='books')
 
 book_router = routers.NestedSimpleRouter(default_router, r'authors', lookup='author')
 book_router.register(r'books', BookViewSet, basename='books')
@@ -19,5 +20,5 @@ urlpatterns = [
     path(r'', include(default_router.urls)),
     path(r'', include(book_router.urls)),
     path(r'', include(country_router.urls)),
-    # path(r'', include(books_filter_router.urls)),
+    path(r'books/', BookFilterViewSet.as_view(), name = 'books')
 ]
--- molodkina_books/views.py
+++ molodkina_books/views.py
@@ -6,6 +6,7 @@ from django.http.response import JsonResponse
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from url_filter.integrations.drf import DjangoFilterBackend
 from django_filters import rest_framework as filters
+from rest_framework import generics
 
 from molodkina_books.models import Book, Author, Country
 from molodkina_books.serializers import BookSerializer, AuthorSerializer, CountrySerializer
@@ -42,15 +43,40 @@ class BookViewSet(ViewSet):
         return JsonResponse(serializer.data, safe=False)
 
 class CountryViewSet(ViewSet):
-    serializer_class = CountrySerializer
 
     def list(self, request, author_pk=None):
         queryset = Country.objects.filter(author=author_pk)
-        serializer = CountrySerializer(queryset, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        serializer_class = CountrySerializer(queryset, many=True)
+        return JsonResponse(serializer_class.data, safe=False)
  
-class BookFilterViewSet(ModelViewSet):
+class BookFilterViewSet(generics.GenericAPIView):
     queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    serializer_class = BookSerializer(queryset, many = True)
     filter_backends = [DjangoFilterBackend]
     filter_fields = ['Language', 'YearOfPublication']
+
+    def get(self, request):
+        filtered_queryset = self.filter_queryset(self.get_queryset())
+        filtered_serializer_class = BookSerializer(filtered_queryset, many = True)
+        return JsonResponse(filtered_serializer_class.data, safe = False)
+
+
+# class BookFilterViewSetMVS(ModelViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#     filter_backends = [DjangoFilterBackend]
+#     filter_fields = ['Language', 'YearOfPublication']
+
+#     def list(self, request, *args, **kwargs):
+#         filtered_queryset = self.filter_queryset(self.get_queryset())
+#         filtered_serializer_class = BookSerializer(filtered_queryset, many = True)
+#         return JsonResponse(filtered_serializer_class.data, safe = False)
+    
+#     def retrieve(self, request, *args, **kwargs):
+#         filtered_queryset = self.filter_queryset(self.get_queryset())
+#         filtered_serializer_class = BookSerializer(filtered_queryset, many = True)
+#         return JsonResponse(filtered_serializer_class.data, safe = False)
+
+#     def destroy(self, request, *args, **kwargs):
+#         return super().destroy(request, *args, **kwargs)
+
